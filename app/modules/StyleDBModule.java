5030afe7742f7ff55b55ed9c5ed04625ea0d1d4e
howenx <smart.hx@icloud.com>
2015-11-11 11:22:54 +0800

    change module for private and separate DB and Gzip Filter

---
+++ app/modules/StyleDBModule.java
@@ -0,0 +1,80 @@
+package modules;
+
+import com.google.inject.PrivateModule;
+import com.google.inject.Scopes;
+import com.google.inject.name.Names;
+import mapper.*;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.transaction.jdbc.JdbcTransactionFactory;
+import org.mybatis.guice.session.SqlSessionManagerProvider;
+import play.db.DBApi;
+import service.ProdService;
+import service.ProdServiceImpl;
+import service.ThemeService;
+import service.ThemeServiceImpl;
+
+import javax.inject.Inject;
+import javax.inject.Provider;
+import javax.inject.Singleton;
+import javax.sql.DataSource;
+import javax.swing.text.Style;
+
+/**
+ * Style database sqlsession inject.注意,每个库都需要一个PrivateModule,而且有自己独立的Service注入和不同的SQLsession命名绑定
+ * Created by howen on 15/11/11.
+ */
+public class StyleDBModule extends PrivateModule{
+    @Override
+    protected void configure() {
+        install(new org.mybatis.guice.MyBatisModule() {
+            @Override
+            protected void initialize() {
+                environmentId("style");
+                //开启驼峰自动映射
+                mapUnderscoreToCamelCase(true);
+
+                bindDataSourceProviderType(StyleDataSourceProvider.class);
+                bindTransactionFactoryType(JdbcTransactionFactory.class);
+
+                //只针对style数据库的Mapper,不可以将一个Mapper多Module进行Add
+                addMapperClass(BrandsMapper.class);
+                addMapperClass(CatesMapper.class);
+                addMapperClass(ProductsMapper.class);
+                addMapperClass(ThemeMapper.class);
+                addMapperClass(StockMapper.class);
+            }
+        });
+
+        /**
+         * bind SQLsession to isolate the multiple datasources.无须更改
+         */
+        bind(SqlSession.class).annotatedWith(Names.named("style")).toProvider(SqlSessionManagerProvider.class).in(Scopes.SINGLETON);
+        expose(SqlSession.class).annotatedWith(Names.named("style"));
+
+        /**
+         * bind service for controller or other service inject. 绑定style数据库所对应的Service
+         */
+        bind(ProdService.class).to(ProdServiceImpl.class);
+        bind(ThemeService.class).to(ThemeServiceImpl.class);
+
+        //必须expose
+        expose(ThemeService.class);
+        expose(ProdService.class);
+    }
+
+    @Singleton
+    public static class StyleDataSourceProvider implements Provider<DataSource> {
+        final DBApi db;
+
+        @Inject
+        public StyleDataSourceProvider(final DBApi db) {
+            this.db = db;
+        }
+
+
+        @Override
+        public DataSource get() {
+            return db.getDatabase("default").getDataSource();
+        }
+    }
+}
--- public/js/nav.js
+++
@@ -1,9 +0,0 @@
-/**
- * Created by cedric on 15/10/28.
- */
-$(function(){
-    $(".contentA .nav>li").click(function(){
-        $(".contentA .nav>li").removeClass("cur");
-        $(this).addClass("cur");
-    })
-})
