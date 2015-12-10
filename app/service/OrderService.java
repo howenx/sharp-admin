82e370297082207005ab6159bbd418e4740d5c05
tiffany <tiffany@tiffanydeiMac.local>
2015-12-10 17:56:52 +0800

    orderList

---
+++ app/service/OrderService.java
@@ -0,0 +1,12 @@
+package service;
+
+import entity.Order;
+
+import java.util.List;
+
+/**
+ * Created by tiffany on 15/12/10.
+ */
+public interface OrderService {
+    public List<Order> getOrdersAll();
+}
