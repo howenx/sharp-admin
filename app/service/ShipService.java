569b9880224348fb3b21b689b356477ebab874e8
tiffany <tiffany@tiffanydeiMac.local>
2015-12-15 13:09:23 +0800

    orderList

--- app/service/ShipService.java
+++ app/service/ShipService.java
@@ -6,5 +6,10 @@ import entity.Ship;
  * Created by tiffany on 15/12/14.
  */
 public interface ShipService {
+    /**
+     * 由订单编号获取物流
+     * @param orderId
+     * @return
+     */
     public Ship getShipByOrderId(Long orderId);
 }
--- app/service/ShipServiceImpl.java
+++ app/service/ShipServiceImpl.java
@@ -3,10 +3,13 @@ package service;
 import entity.Ship;
 import mapper.ShipMapper;
 
+import javax.inject.Inject;
+
 /**
  * Created by tiffany on 15/12/14.
  */
 public class ShipServiceImpl implements ShipService{
+    @Inject
     private ShipMapper shipMapper;
     @Override
     public Ship getShipByOrderId(Long orderId) {return shipMapper.getShipByOrderId(orderId);}
