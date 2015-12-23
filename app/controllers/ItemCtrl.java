e6050f1760c9d2862393e9913378f5e330850b31
sunny <sunny.wu@kakaocorp.com>
2015-12-23 08:52:11 +0800

    ItemCtrl change

--- app/controllers/ItemCtrl.java
+++ app/controllers/ItemCtrl.java
@@ -2,7 +2,6 @@ package controllers;
 
 import com.fasterxml.jackson.databind.JsonNode;
 import entity.*;
-import order.GetLogistics;
 import play.Logger;
 import play.i18n.Lang;
 import play.i18n.Messages;
@@ -14,7 +13,10 @@ import service.*;
 
 import javax.inject.Inject;
 import java.math.BigDecimal;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 /**
  * 商品管理
@@ -74,7 +76,7 @@ public class ItemCtrl extends Controller {
         item.setOffset(0);
         Logger.error("所有商品:"+service.itemSearch(item).toString());
 
-        return ok(views.html.item.itemsearch.render(lang,ThemeCtrl.IMAGE_URL,ThemeCtrl.PAGE_SIZE,countNum,pageCount,service.itemSearch(item),(User) ctx().args.get("user")));
+        return ok(views.html.item.itemsearch.render(lang,"http://hmm-images.oss-cn-beijing.aliyuncs.com/",ThemeCtrl.PAGE_SIZE,countNum,pageCount,service.itemSearch(item),(User) ctx().args.get("user")));
     }
 
     /**
@@ -180,7 +182,7 @@ public class ItemCtrl extends Controller {
             invList.add(object);
         }
 
-        return ok(views.html.item.itemdetail.render(item,invList,cates,pCateNm,brands,ThemeCtrl.IMAGE_URL,lang,(User) ctx().args.get("user")));
+        return ok(views.html.item.itemdetail.render(item,invList,cates,pCateNm,brands,"http://hmm-images.oss-cn-beijing.aliyuncs.com/",lang,(User) ctx().args.get("user")));
     }
 
     /**
@@ -235,7 +237,7 @@ public class ItemCtrl extends Controller {
             object[24] = inventory.getRecordCode();
             invList.add(object);
         }
-        return ok(views.html.item.itemupdate.render(item,invList,cates,pCateNm,brands,ThemeCtrl.IMAGE_URL,lang,service.getAllBrands(),service.getParentCates(),carriageService.getModels(),(User) ctx().args.get("user")));
+        return ok(views.html.item.itemupdate.render(item,invList,cates,pCateNm,brands,"http://hmm-images.oss-cn-beijing.aliyuncs.com/",lang,service.getAllBrands(),service.getParentCates(),carriageService.getModels(),(User) ctx().args.get("user")));
     }
 
     /**
