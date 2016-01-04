08ee143391a282b88b99a54d83cee63633fb110e
tiffany <tiffany@tiffanydeiMac.local>
2016-01-04 19:31:26 +0800

    theme and order

--- app/mapper/OrderMapper.java
+++ app/mapper/OrderMapper.java
@@ -8,9 +8,30 @@ import java.util.List;
  * Created by tiffany on 15/12/10.
  */
 public interface OrderMapper {
-
+    /**
+     * 订单列表     Added by Tiffany Zhu
+     * @return
+     */
     List<Order> getOrdersAll();
+
+    /**
+     * ajax分页查询     Added by Tiffany Zhu
+     * @param order
+     * @return
+     */
     List<Order> getOrderPage(Order order);
+
+    /**
+     * 由订单ID获取订单详情      Added by Tiffany Zhu
+     * @param orderId
+     * @return
+     */
     Order getOrderById(Long orderId);
+
+    /**
+     * 取消支付超时的订单 Added  by Tiffany Zhu
+     * @param orderId
+     */
+    void orderCancel(Long orderId);
 
 }
--- app/service/OrderServiceImpl.java
+++ app/service/OrderServiceImpl.java
@@ -10,17 +10,40 @@ import java.util.List;
  * Created by tiffany on 15/12/10.
  */
 public class OrderServiceImpl implements OrderService {
-
     @Inject
     private OrderMapper orderMapper;
+
+    /**
+     * 订单列表     Added by Tiffany Zhu
+     * @return
+     */
     @Override
     public List<Order> getOrdersAll(){return orderMapper.getOrdersAll();}
 
+    /**
+     * 订单ajax查询     Added by Tiffany Zhu
+     * @param order
+     * @return
+     */
     @Override
     public List<Order> getOrderPage(Order order) {
         return orderMapper.getOrderPage(order);
     }
 
+    /**
+     * 由订单ID订单详情   Added by Tiffany Zhu
+     * @param orderId
+     * @return
+     */
     @Override
     public Order getOrderById(Long orderId) {return orderMapper.getOrderById(orderId);}
+
+    /**
+     * 取消支付超时的订单 Added  by Tiffany Zhu
+     * @param orderId
+     */
+    @Override
+    public void orderCancel(Long orderId) {
+        orderMapper.orderCancel(orderId);
+    }
 }
