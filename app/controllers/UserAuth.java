4cb7ffc29c019b1e94c2c43155cb12fe5affdd9b
howen <smart.hx@icloud.com>
2016-01-29 19:32:30 +0800

    ddd

--- app/controllers/UserAuth.java
+++ app/controllers/UserAuth.java
@@ -1,12 +1,17 @@
 package controllers;
 
 
+import com.typesafe.config.ConfigFactory;
 import entity.User;
+import play.Configuration;
 import play.Logger;
 import play.cache.Cache;
 import play.mvc.Http;
 import play.mvc.Result;
 import play.mvc.Security;
+
+import javax.inject.Inject;
+import java.util.Optional;
 
 /**
  * Created by handy on 15/11/3.
@@ -14,22 +19,33 @@ import play.mvc.Security;
  */
 public class UserAuth extends Security.Authenticator {
 
+    @Inject
+    Configuration configuration;
+
     @Override
     public String getUsername(Http.Context ctx) {
-        String username =  ctx.session().get("username");
-        if(username != null) {
-            Logger.debug("userAuth user " + username);
-            User user = (User) Cache.get(username.trim());
-            if(user != null) {
-                ctx.args.put("user",user);
-            }
-            else {
-                return null;
-            }
+
+        Optional<String> header = Optional.ofNullable(ctx.request().getHeader("Referer"));
+        if (header.isPresent()) {
+
+            String username =  ctx.session().get("username");
+            if(username != null) {
+                Logger.debug("userAuth user " + username);
+                User user = (User) Cache.get(username.trim());
+                configuration.getStringList()
 
 
-        }
-        return username;
+                if(user != null) {
+                    ctx.args.put("user",user);
+                }
+                else {
+                    return null;
+                }
+            }
+            return username;
+        }else return null;
+
+
     }
 
 
