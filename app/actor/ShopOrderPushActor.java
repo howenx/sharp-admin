e47aa06aad1fe937cbe8c40509206fcf65895b9d
sunny <sunny.wu@kakaocorp.com>
2016-03-07 15:36:40 +0800

    ShopOrderCreate

---
+++ app/actor/ShopOrderPushActor.java
@@ -0,0 +1,27 @@
+package actor;
+
+import akka.actor.AbstractActor;
+import akka.japi.pf.ReceiveBuilder;
+import middle.ShopOrderPushMiddle;
+import play.Logger;
+
+import javax.inject.Inject;
+
+/**
+ * Created by Sunny Wu on 16/3/7.
+ * kakao china.
+ */
+public class ShopOrderPushActor extends AbstractActor{
+
+    @Inject
+    public ShopOrderPushActor(ShopOrderPushMiddle shopOrderPushMiddle) {
+        receive(ReceiveBuilder.match(Long.class, orderId -> {
+            shopOrderPushMiddle.shopOrderPush(orderId);
+            Logger.debug("order"+orderId+":push to ERP");
+        }).matchAny(s-> {
+            Logger.error("push to ERP error!", s.toString());
+            unhandled(s);
+        }).build());
+
+    }
+}
--- app/mapper/OrderLineMapper.java
+++ app/mapper/OrderLineMapper.java
@@ -9,9 +9,16 @@ import java.util.List;
  */
 public interface OrderLineMapper {
     /**
-     * 由订单Id获取订单商品
+     * 由订单拆单Id获取订单商品
      * @param splitId
      * @return
      */
     List<OrderLine> getLineBySplitId(Long splitId);
+
+    /**
+     * 由订单id获取订单商品          Added By Sunny.Wu  2016.03.07
+     * @param orderId 订单id
+     * @return List of OrderLine
+     */
+    List<OrderLine> getLineByOrderId(Long orderId);
 }
