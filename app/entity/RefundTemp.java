2ca1c85e16b5d8a59fec07ca2b1b18d02af91d17
tiffany <tiffany@tiffanydeiMac.local>
2016-04-16 11:51:31 +0800

    refund

--- app/entity/RefundTemp.java
+++ app/entity/RefundTemp.java
@@ -1,5 +1,6 @@
 package entity;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import play.data.validation.Constraints;
 
 import java.io.Serializable;
@@ -10,36 +11,54 @@ import java.security.Timestamp;
  * 退款VO
  * Created by howen on 15/12/22.
  */
-public class Refund implements Serializable{
+public class RefundTemp implements Serializable{
 
+
+
+    @JsonIgnore
     private Long        id;//主键
     @Constraints.Required
     private Long        orderId;//订单ID
     @Constraints.Required
     private Long        splitOrderId;//子订单ID
+    @JsonIgnore
     private Long        skuId;//商品ID
+    @JsonIgnore
     private String      skuType;//商品类型
+    @JsonIgnore
     private Long        skuTypeId;//商品类型ID
     @Constraints.Required
     private BigDecimal  payBackFee;//退款金额
     @Constraints.Required
     private String      reason;//申请退款原因
     private String      state;//状态
+    @JsonIgnore
     private String      pgTradeNo;//支付流水号
+    @JsonIgnore
     private String      pgCode;//支付返回码
+    @JsonIgnore
     private String      pgMessage;//支付返回消息
 
+    @JsonIgnore
     private Timestamp   createAt;//创建时间
+    @JsonIgnore
     private Timestamp   updateAt;//更新时间
+    @JsonIgnore
     private Integer     amount;//申请退款数量
+    @JsonIgnore
     private String      refundImg;//退款上传图片
+    @JsonIgnore
     private String      contactName;//联系人姓名
     private String      contactTel;//联系人电话
+    @JsonIgnore
     private String      expressCompany;//快递公司名称
+    @JsonIgnore
     private String      expressCompCode;//快递公司编码
+    @JsonIgnore
     private String      expressNum;//快递编号
     private String      rejectReason;//客服拒绝退款原因
 
+    @JsonIgnore
     private Long        userId;//用户ID
     @Constraints.Required
     private String      refundType;//退款类型，pin：拼购自动退款，receive：收货后申请退款，deliver：发货前退款
@@ -55,10 +74,10 @@ public class Refund implements Serializable{
     private String order;
 
 
-    public Refund() {
+    public RefundTemp() {
     }
 
-    public Refund(Long id, Long orderId, Long splitOrderId, Long skuId, String skuType, Long skuTypeId, BigDecimal payBackFee, String reason, String state, String pgTradeNo, String pgCode, String pgMessage, Timestamp createAt, Timestamp updateAt, Integer amount, String refundImg, String contactName, String contactTel, String expressCompany, String expressCompCode, String expressNum, String rejectReason, Long userId, String refundType, Integer pageSize, Integer offset, String sort, String order) {
+    public RefundTemp(Long id, Long orderId, Long splitOrderId, Long skuId, String skuType, Long skuTypeId, BigDecimal payBackFee, String reason, String state, String pgTradeNo, String pgCode, String pgMessage, Timestamp createAt, Timestamp updateAt, Integer amount, String refundImg, String contactName, String contactTel, String expressCompany, String expressCompCode, String expressNum, String rejectReason, Long userId, String refundType, Integer pageSize, Integer offset, String sort, String order) {
         this.id = id;
         this.orderId = orderId;
         this.splitOrderId = splitOrderId;
@@ -91,7 +110,7 @@ public class Refund implements Serializable{
 
     @Override
     public String toString() {
-        return "Refund{" +
+        return "RefundTemp{" +
                 "id=" + id +
                 ", orderId=" + orderId +
                 ", splitOrderId=" + splitOrderId +
@@ -121,6 +140,10 @@ public class Refund implements Serializable{
                 ", sort='" + sort + '\'' +
                 ", order='" + order + '\'' +
                 '}';
+    }
+
+    public static long getSerialVersionUID() {
+        return serialVersionUID;
     }
 
     public Long getId() {
