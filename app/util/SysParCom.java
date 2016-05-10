6efa9ae4afb03d26a067126d14175f5a0d96a010
howen <smart.hx@icloud.com>
2016-05-11 02:45:01 +0800

    asdf

--- app/util/SysParCom.java
+++ app/util/SysParCom.java
@@ -2,9 +2,13 @@ package util;
 
 import com.squareup.okhttp.OkHttpClient;
 import play.Configuration;
+import redis.clients.jedis.JedisPubSub;
 
 import javax.inject.Inject;
 import javax.inject.Singleton;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 /**
  * 查询参数表中的参数项
@@ -18,6 +22,8 @@ public class SysParCom {
     public static String REDIS_PASSWORD;
     public static Integer REDIS_PORT;
     public static String REDIS_CHANNEL;
+
+    public static List<String> REDIS_SUBSCRIBE;
 
     //图片服务器url
     public static String IMAGE_URL;
@@ -36,20 +42,24 @@ public class SysParCom {
 
     public static String EXPRESS_CUSTOMER;
 
+    public static Map<String,JedisPubSub> JEDIS_SUB;
+
+
     public static final OkHttpClient client = new OkHttpClient();
 
     @Inject
     public SysParCom(Configuration configuration) {
-
+        JEDIS_SUB = new HashMap<>();
         REDIS_URL = configuration.getString("redis.host");
         REDIS_PASSWORD = configuration.getString("redis.password");
         REDIS_PORT = configuration.getInt("redis.port");
         REDIS_CHANNEL = configuration.getString("redis.channel");
+        REDIS_SUBSCRIBE = configuration.getStringList("redis.subscribe");
         IMAGE_URL = configuration.getString("image.server.url");
         DEPLOY_URL = configuration.getString("deploy.server.url");
         IMG_UPLOAD_URL = configuration.getString("image.upload.url");
         ORDER_QUERY_DELAY = configuration.getInt("shop.order.query.delay");
-        ORDER_QUERY_INTERVAL =configuration.getInt("shop.order.query.interval");
+        ORDER_QUERY_INTERVAL = configuration.getInt("shop.order.query.interval");
         EXPRESS_POST_URL = configuration.getString("express.post.url");
         EXPRESS_KEY = configuration.getString("express.key");
         EXPRESS_CUSTOMER = configuration.getString("express.customer");
