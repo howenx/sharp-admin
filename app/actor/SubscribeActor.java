fc79b2a27baa589e53bbec5806dff1d8a732c49b
howen <smart.hx@icloud.com>
2016-05-11 19:23:08 +0800

    asdf

--- app/actor/SubscribeActor.java
+++ app/actor/SubscribeActor.java
@@ -4,10 +4,16 @@ import akka.actor.AbstractActor;
 import akka.japi.pf.ReceiveBuilder;
 import play.Logger;
 import play.mvc.WebSocket;
-import redis.clients.jedis.Jedis;
 import redis.clients.jedis.JedisPool;
 import redis.clients.jedis.JedisPubSub;
 import util.RedisListener;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.stream.Collectors;
 
 import static util.SysParCom.JEDIS_SUB;
 import static util.SysParCom.REDIS_SUBSCRIBE;
@@ -18,27 +24,39 @@ import static util.SysParCom.REDIS_SUBSCRIBE;
  */
 public class SubscribeActor extends AbstractActor {
 
+    private Callable<Boolean> callable(JedisPool jedisPool, WebSocket.Out<String> out, String channel) {
+        return () -> {
+            JedisPubSub listener = new RedisListener(out);
+            JEDIS_SUB.put(channel, listener);
+            System.out.println("hmm." + channel);
+            jedisPool.getResource().psubscribe(listener, "hmm." + channel);
+            return listener.isSubscribed();
+        };
+    }
+
     public SubscribeActor(JedisPool jedisPool, WebSocket.In<String> in, WebSocket.Out<String> out) {
         receive(ReceiveBuilder.match(String.class, message -> {
             try {
-
-                Jedis jedis = jedisPool.getResource();
-
+                ExecutorService executor = Executors.newFixedThreadPool(REDIS_SUBSCRIBE.size());
+                List<Callable<Boolean>> callables = new ArrayList<>();
+                System.err.println(REDIS_SUBSCRIBE);
                 if (message.equals("start")) {
-                    for (String channel : REDIS_SUBSCRIBE) {
-                        JedisPubSub listener = new RedisListener(out);
-                        JEDIS_SUB.put(channel, listener);
-                        jedis.psubscribe(listener, "hmm."+channel);
-                    }
-                } else if (message.equals("end")) {
-                    for (String channel : REDIS_SUBSCRIBE) {
-                        JedisPubSub listener = JEDIS_SUB.get(channel);
-                        listener.unsubscribe();
-                    }
-                }
+                    callables.addAll(REDIS_SUBSCRIBE.stream().map(channel -> callable(jedisPool, out, channel)).collect(Collectors.toList()));
 
-//                ExecutorService executor = Executors.newSingleThreadExecutor();
-//                executor.submit(() -> jedis.psubscribe(listener, channel));
+                    executor.invokeAll(callables)
+                            .stream()
+                            .map(future -> {
+                                try {
+                                    return future.get();
+                                } catch (Exception e) {
+                                    throw new IllegalStateException(e);
+                                }
+                            })
+                            .forEach(System.out::println);
+
+                } else if (message.equals("end")) {
+                    executor.shutdown();
+                }
 
             } catch (Exception ignored) {
             }
