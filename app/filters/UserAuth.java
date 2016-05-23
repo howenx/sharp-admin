66cab47f75b774d0c9a59b4c4f9a6f0cebbbe2a9
sunny <sunny.wu@kakaocorp.com>
2016-05-23 16:45:50 +0800

    user

--- app/filters/UserAuth.java
+++ app/filters/UserAuth.java
@@ -35,14 +35,13 @@ public class UserAuth extends Security.Authenticator {
         Optional<String> header = Optional.ofNullable(ctx.request().uri());
         //method
         Optional<String> header2 = Optional.of(ctx._requestHeader().tags().get(Routes.ROUTE_CONTROLLER).get()+"."+ctx._requestHeader().tags().get(Routes.ROUTE_ACTION_METHOD).get());
-        Logger.debug("url:"+header.get());
-        Logger.debug("method:"+header2.get());
+//        Logger.info("url:"+header.get());
+//        Logger.info("method:"+header2.get());
         if (header.isPresent()) {
             String username =  ctx.session().get("username");
             if(username != null) {
                 User user = (User) Cache.get(username.trim());
                 if(user != null) {
-//                    Logger.error("用户shi..........:"+user.toString());
                     Logger.info("user:"+user.userType().get());
                     //记录用户行为
                     if (configuration.getStringList("userOperate").contains(header2.get())){
@@ -64,11 +63,15 @@ public class UserAuth extends Security.Authenticator {
                         userLogService.insertUserLog(userLog);
                     }
                     //过滤用户权限
-                    if (configuration.getStringList(String.valueOf(user.userType().get())).contains(header.get())){
-                        ctx.args.put("user",user);
-                    } else if (configuration.getStringList(String.valueOf(user.userType().get())).contains(header2.get())) {
-                        ctx.args.put("user",user);
-                    } else return null;
+                    String[] userTypeArr = user.userType().get().split(",");
+                    Boolean flag = false;
+                    for (String userType : userTypeArr) {
+                        if (configuration.getStringList(userType).contains(header2.get())) {
+                            flag = true;
+                        }
+                    }
+                    if (flag) ctx.args.put("user",user);
+                    else return null;
                 }
                 else {
                     return null;
