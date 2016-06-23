23f852aff9f3cd9ba1128e47579ac70969fe70cd
howen <smart.hx@icloud.com>
2016-06-23 18:49:40 +0800

    asfd

--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-./activator clean stage
+./activator-1.3.10-minimal/bin/activator clean stage
 echo '123123'|sudo -S kill -9 `sudo lsof -t -i:9002`
 echo '123123'|sudo -S rm -rf target/universal/stage/RUNNING_PID
 target/universal/stage/bin/style-admin -Dhttp.port=9002 -Dconfig.file=target/universal/stage/conf/application.conf -Dlogger.resource=logprod.xml
