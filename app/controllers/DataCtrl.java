ef6215e1f1bf1a95b23a5c0157ee05fffce1119b
sunny <sunny.wu@kakaocorp.com>
2016-07-06 18:28:31 +0800

    sales data sql bug fix

--- app/controllers/DataCtrl.java
+++ app/controllers/DataCtrl.java
@@ -198,18 +198,14 @@ public class DataCtrl extends Controller {
                 BigDecimal expend = new BigDecimal(0.0);    //退款额
                 BigDecimal profit = new BigDecimal(0.0);    //收入
                 for(Order o : orderList) {
-
                     if ("pin".equals(o.getOrderStatus()) || "receive".equals(o.getOrderStatus()) || "deliver".equals(o.getOrderStatus())) {
-                        Logger.error("订单:"+o.toString());
                         //退款单, 退款额增加, 收入减少
                         Refund refund = refundService.getRefundByOrderId(o.getOrderId());
-                        Logger.error("退款单:"+refund.toString());
                         BigDecimal payBackFee = refund.getPayBackFee();
                         expend = expend.add(payBackFee);
                         profit = profit.subtract(payBackFee);
                     }
                     if ("S".equals(o.getOrderStatus()) || "R".equals(o.getOrderStatus()) || "D".equals(o.getOrderStatus()) || "J".equals(o.getOrderStatus()) || "PS".equals(o.getOrderStatus())) {
-
                         //付款单, 收款额增加, 收入增加
                         BigDecimal payTotal = o.getPayTotal();
                         income = income.add(payTotal);
