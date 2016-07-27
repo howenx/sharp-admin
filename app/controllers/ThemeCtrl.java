5ab3508f4eb6c593299c4490055ea510a2e838ae
tiffany <tiffany@tiffanydeiMac.local>
2016-07-27 17:53:16 +0800

    category

--- app/controllers/ThemeCtrl.java
+++ app/controllers/ThemeCtrl.java
@@ -110,6 +110,36 @@ public class ThemeCtrl extends Controller {
         return ok(views.html.theme.category.render(lang,sliderList, SysParCom.IMAGE_URL,SysParCom.IMG_UPLOAD_URL,(User) ctx().args.get("user")));
     }
 
+    /**
+     * 滚动条弹窗  主题列表和商品列表
+     * @return sliderPop.scala.html
+     */
+    @Security.Authenticated(UserAuth.class)
+    public Result categoryPop(){
+        //主题列表
+        List<Theme> themeList = service.getThemesAll();
+        List<Theme> tList = new ArrayList<>();
+        String strNow = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(new Date());//现在时间
+        for(Theme theme : themeList) {
+            if (theme.getEndAt().compareTo(strNow)>0) {
+                theme.setThemeImg(Json.parse(theme.getThemeImg()).get("url").asText());
+                tList.add(theme);
+            }
+        }
+        List<Skus> skusList = inventoryService.getAllSkus();
+        List<Skus> slist = new ArrayList<>();
+        for(Skus skus : skusList) {//商品列表为(除自定义价格和多样化价格的预售和正常商品)
+            if (!skus.getSkuType().equals("customize")&&!skus.getSkuType().equals("vary")&&(skus.getSkuTypeStatus().equals("P")||skus.getSkuTypeStatus().equals("Y"))) {
+                skus.setSkuTypeImg(Json.parse(skus.getSkuTypeImg()).get("url").asText());
+                slist.add(skus);
+            }
+        }
+        if (themeList.size()>0 && skusList.size()>0) {
+            return ok(views.html.theme.categoryPop.render(tList,slist,SysParCom.IMAGE_URL));
+        }
+        else
+            return ok("没有数据");
+    }
 
     /**
      * 主题录入
