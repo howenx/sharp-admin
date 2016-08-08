80e21044b5ef7917cd694913d130178352be4e6b
tiffany <tiffany.zhu@kakaocorp.com>
2016-08-08 17:34:41 +0800

    bug fix

--- public/js/shot.style.js
+++ public/js/shot.style.js
@@ -200,7 +200,9 @@ $(function() {
                           customizeObject.id =  $(this).find("td:eq(3)").text();
                       }
                       customizeObject.invId = $(this).find("td:eq(1)").text();
-                      customizeObject.state = $(this).find("td:eq(7)").text();
+                      if($(this).find("td:eq(7)").text() == "正常"){
+                             customizeObject.state = "Y";
+                      }
                       customizeObject.price = $(this).find("td:eq(8)").text();
                       customizeObject.discount = $(this).find("td:eq(10)").text();
                       customizeItems.push(customizeObject);
